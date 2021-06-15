d4f7426e1982acc9398c2eefd76293677a3244ec
rcwhit80 <rcwhit80@yahoo.com>
2021-06-15 16:32:12 -0500

    updated files

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,6 @@
 library(data.table)
 library(dplyr)
 
-
 ## Download the raw data file
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
